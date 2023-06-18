2b912ac3b36f512e2a1b2f04a2765b4886a6dc13
Dmytro Chyrva <dmytro.chyrva@protonmail.com>
2023-06-19 00:13:54 +0200

    [00000] Initial Tables Created.

---
+++ Seeds/1. DISHES_SEEDS.sql
@@ -0,0 +1,94 @@
+INSERT INTO
+  DISHES (NAME, PRICE, AVAILABLE, CATEGORY_ID, IMAGE_URL)
+VALUES
+  (
+    N'Spicy seasoned seafood noodles',
+    2.29,
+    20,
+    1,
+    N'http://localhost:4200/assets/images/mocks/image.png'
+  ),
+  (
+    N'Salted Pasta with mushroom sauce',
+    2.69,
+    11,
+    1,
+    N'http://localhost:4200/assets/images/mocks/image1.png'
+  ),
+  (
+    N'Beef dumpling in hot and sour soup',
+    2.99,
+    16,
+    1,
+    N'http://localhost:4200/assets/images/mocks/image2.png'
+  ),
+  (
+    N'Healthy noodle with spinach leaf',
+    3.29,
+    22,
+    1,
+    N'http://localhost:4200/assets/images/mocks/image3.png'
+  ),
+  (
+    N'Hot spicy fried rice with omelet',
+    3.49,
+    13,
+    1,
+    N'http://localhost:4200/assets/images/mocks/image4.png'
+  ),
+  (
+    N'Spicy instant noodle with special omelette',
+    3.59,
+    17,
+    1,
+    N'http://localhost:4200/assets/images/mocks/image5.png'
+  ),
+  (
+    N'Cold fried rice with omelet',
+    4.59,
+    14,
+    1,
+    N'http://localhost:4200/assets/images/mocks/image1.png'
+  ),
+  (
+    N'Fried rice with omelet',
+    2.59,
+    11,
+    1,
+    N'http://localhost:4200/assets/images/mocks/image4.png'
+  ),
+  (
+    N'Noodle with omelette',
+    1.59,
+    8,
+    1,
+    N'http://localhost:4200/assets/images/mocks/image3.png'
+  ),
+  (
+    N'Noodle with omelette',
+    1.10,
+    2,
+    2,
+    N'http://localhost:4200/assets/images/mocks/image3.png'
+  ),
+  (
+    N'Fried rice with omelet',
+    4.60,
+    7,
+    2,
+    N'http://localhost:4200/assets/images/mocks/image2.png'
+  ),
+  (
+    N'Spicy instant noodle with special omelette',
+    3.59,
+    17,
+    3,
+    N'http://localhost:4200/assets/images/mocks/image5.png'
+  ),
+  (
+    N'Cold fried rice with omelet',
+    4.59,
+    14,
+    3,
+    N'http://localhost:4200/assets/images/mocks/image1.png'
+  );
---
+++ Tables/1. CATEGORIES.sql
@@ -0,0 +1,12 @@
+CREATE TABLE
+  CATEGORIES (ID INT PRIMARY KEY, NAME VARCHAR(16));
+
+INSERT INTO
+  CATEGORIES (ID, NAME)
+VALUES
+  (1, 'Hot Dishes'),
+  (2, 'Cold Dishes'),
+  (3, 'Soup'),
+  (4, 'Grill'),
+  (5, 'Appetizer'),
+  (6, 'Desert');
